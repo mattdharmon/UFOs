b1dd31440b12e76b08e0ad88ede4109d600ffd26
Matthew Harmon <harmon.matthewd@gmail.com>
2021-03-14 10:24:32 -0500

    Added structure and first js function for the project.

---
+++ static/js/app.js
@@ -0,0 +1,28 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+/**
+ * Build out the visual table for the ufo data.
+ * 
+ * @param {Array} data 
+ */
+function buildTable(data) {
+    // Clear the table
+    tbody.html("");
+
+    // Loop through each field in the dataRow and
+    // add each value as a table cell (td)
+    data.forEach((dataRow) => {
+        // Create a table row.
+        let row = tbody.append("tr");
+
+        // Add data cells to the row.
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+        });
+    });
+}
